097b137f1a0e4c8b06855c168107b501e419e71d
Kai Blin <kai@samba.org>
2011-08-11 10:46:28 +0200

    Initial commit of Flask-Downloader

---
+++ flaskext/downloader.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    flaskext.downloader
+    ~~~~~~~~~~~~~~~~~~~
+
+    Description of the module goes here...
+
+    :copyright: (c) 2011 by Kai Blin.
+    :license: BSD, see LICENSE for more details.
+"""
