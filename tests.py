810dd9de1f2ae7501f6ed431b6d0db1e127751aa
Kai Blin <kai@samba.org>
2011-08-12 10:56:19 +0200

    Add more tests for download function

--- tests.py
+++ tests.py
@@ -15,6 +15,10 @@ class DownloaderTest(unittest.TestCase):
 
     def setUp(self):
         self.DEFAULT_DOWNLOAD_DIR = tempfile.mkdtemp()
+        (fd, name) = tempfile.mkstemp(dir=self.DEFAULT_DOWNLOAD_DIR)
+        os.write(fd, 'Test file\n')
+        os.close(fd)
+        self.tmp_name = name
         self.app = Flask(__name__)
         self.app.config.from_object(self)
 
@@ -26,7 +30,15 @@ class DownloaderTest(unittest.TestCase):
         shutil.rmtree(self.DEFAULT_DOWNLOAD_DIR)
 
     def test_download(self):
-        """Test download function"""
+        """Test download function downloads file from URL
+        """
+        res = self.dl.download(self.tmp_name)
+        assert res is not None
+        assert hasattr(res, 'save')
+
+    def test_download_returns_none_on_failure(self):
+        """Test download function returns None on failure
+        """
         res = self.dl.download(path.join(
                         self.DEFAULT_DOWNLOAD_DIR,'invalid.txt'))
         assert res is None
